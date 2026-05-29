f790bf5b00b40e23da06e8010e5c9ea9ca3b66d2
Milliromeck <darinaromanovskaa90@gmail.com>
2026-05-29 22:31:45 +0300

    Завершила команду search с имитацией вывода статьи

--- cli/bin/cli.dart
+++ cli/bin/cli.dart
@@ -16,7 +16,9 @@ void searchWikipedia(List<String>? arguments) {
     articleTitle = arguments.join(' ');
   }
 
-  print('Current article title: $articleTitle');
+  print('Looking up articles about "$articleTitle". Please wait.');
+  print('Here ya go!');
+  print('(Pretend this is an article about "$articleTitle")');
 }
 
 void main(List<String> arguments) {
