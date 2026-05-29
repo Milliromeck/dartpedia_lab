85ac069650a60242a30854c906b3a54788af32ea
Milliromeck <darinaromanovskaa90@gmail.com>
2026-05-29 22:46:54 +0300

    Обновила экспорт wikipedia с API функциями

--- wikipedia/lib/wikipedia.dart
+++ wikipedia/lib/wikipedia.dart
@@ -4,3 +4,5 @@ export 'src/model/summary.dart';
 export 'src/model/title_set.dart';
 export 'src/model/article.dart';
 export 'src/model/search_results.dart';
+export 'src/api/summary.dart';
+export 'src/api/search.dart';
