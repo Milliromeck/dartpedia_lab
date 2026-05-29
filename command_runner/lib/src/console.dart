0559b5fc79d795915e8f9ad4c2b70ee82cde8030
Milliromeck <darinaromanovskaa90@gmail.com>
2026-05-29 22:52:00 +0300

    Добавила цветной вывод в консоль (ConsoleColor)

---
+++ command_runner/lib/src/console.dart
@@ -0,0 +1,27 @@
+import 'dart:io';
+
+const String ansiEscapeLiteral = '\x1B';
+
+enum ConsoleColor {
+  lightBlue(184, 234, 254),
+  red(242, 93, 80),
+  yellow(249, 248, 196),
+  grey(240, 240, 240),
+  white(255, 255, 255);
+
+  const ConsoleColor(this.r, this.g, this.b);
+  final int r, g, b;
+
+  String get enableForeground => '$ansiEscapeLiteral[38;2;$r;$g;${b}m';
+  String get enableBackground => '$ansiEscapeLiteral[48;2;$r;$g;${b}m';
+  static String get reset => '$ansiEscapeLiteral[0m';
+
+  String applyForeground(String text) =>
+      '$ansiEscapeLiteral[38;2;$r;$g;${b}m$text$reset';
+}
+
+extension TextRenderUtils on String {
+  String get errorText => ConsoleColor.red.applyForeground(this);
+  String get instructionText => ConsoleColor.yellow.applyForeground(this);
+  String get titleText => ConsoleColor.lightBlue.applyForeground(this);
+}
