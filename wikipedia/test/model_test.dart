42f03b37d4250ad79edd897739f0a1f8cd55ce3f
Milliromeck <darinaromanovskaa90@gmail.com>
2026-05-29 22:53:52 +0300

    Добавила тесты для моделей Article и SearchResults

--- wikipedia/test/model_test.dart
+++ wikipedia/test/model_test.dart
@@ -1,7 +1,8 @@
 import 'dart:convert';
-import 'dart:io';
 import 'package:test/test.dart';
-import 'package:wikipedia/wikipedia.dart';
+import 'package:wikipedia/src/model/summary.dart';
+import 'package:wikipedia/src/model/article.dart';
+import 'package:wikipedia/src/model/search_results.dart';
 
 void main() {
   group('Summary model', () {
@@ -20,7 +21,36 @@ void main() {
       };
       final summary = Summary.fromJson(json);
       expect(summary.titles.canonical, 'Dart_(programming_language)');
-      expect(summary.extract, 'Dart is a programming language');
+    });
+  });
+
+  group('Article model', () {
+    test('listFromJson returns list of articles', () {
+      final json = {
+        'query': {
+          'pages': {
+            '123': {'title': 'Cat', 'extract': 'Cat is a pet'},
+            '456': {'title': 'Dog', 'extract': 'Dog is a pet'}
+          }
+        }
+      };
+      final articles = Article.listFromJson(json);
+      expect(articles.length, 2);
+      expect(articles.first.title, 'Cat');
+    });
+  });
+
+  group('SearchResults model', () {
+    test('fromJson parses opensearch response', () {
+      final json = [
+        'dart',
+        ['Dart', 'Dartmouth'],
+        ['', ''],
+        ['https://en.wikipedia.org/Dart', 'https://en.wikipedia.org/Dartmouth']
+      ];
+      final results = SearchResults.fromJson(json);
+      expect(results.results.length, 2);
+      expect(results.results.first.title, 'Dart');
     });
   });
 }
