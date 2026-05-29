0f09a82e4897fc6b2405d06829606557a5be5d90
Milliromeck <darinaromanovskaa90@gmail.com>
2026-05-29 22:32:13 +0300

    Реализовала HTTP-запрос к Wikipedia API (функция getWikipediaArticle)

--- cli/bin/cli.dart
+++ cli/bin/cli.dart
@@ -1,12 +1,28 @@
 import 'dart:io';
+import 'package:http/http.dart' as http;
 
 const version = '0.0.1';
 
 void printUsage() {
-  print("The following commands are valid: 'help', 'version', 'search <ARTICLE-TITLE>'");
+  print("The following commands are valid: 'help', 'version', 'wikipedia <ARTICLE-TITLE>'");
 }
 
-void searchWikipedia(List<String>? arguments) {
+Future<String> getWikipediaArticle(String articleTitle) async {
+  final url = Uri.https(
+    'en.wikipedia.org',
+    '/api/rest_v1/page/summary/$articleTitle',
+  );
+  
+  final response = await http.get(url);
+  
+  if (response.statusCode == 200) {
+    return response.body;
+  }
+  
+  return 'Error: Failed to fetch article "$articleTitle". Status code: ${response.statusCode}';
+}
+
+void searchWikipedia(List<String>? arguments) async {
   final String articleTitle;
 
   if (arguments == null || arguments.isEmpty) {
@@ -17,8 +33,8 @@ void searchWikipedia(List<String>? arguments) {
   }
 
   print('Looking up articles about "$articleTitle". Please wait.');
-  print('Here ya go!');
-  print('(Pretend this is an article about "$articleTitle")');
+  final articleContent = await getWikipediaArticle(articleTitle);
+  print(articleContent);
 }
 
 void main(List<String> arguments) {
@@ -26,7 +42,7 @@ void main(List<String> arguments) {
     printUsage();
   } else if (arguments.first == 'version') {
     print('Dartpedia CLI version $version');
-  } else if (arguments.first == 'search') {
+  } else if (arguments.first == 'wikipedia') {
     final inputArgs = arguments.length > 1 ? arguments.sublist(1) : null;
     searchWikipedia(inputArgs);
   } else {
